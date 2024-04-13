430d92bb0bac0bf4c833f0b221f871dabdeb45dc
Rafaelgbm <rafaelgasparmartins@icloud.com>
2024-04-12 22:44:46 -0300

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-function clicar(){
-    var n1 = document.getElementById('number1').value;
-    var n2 = document.getElementById('number2').value;
+var resultado = document.getElementById('resultado')
 
-    console.log(n1)
-    console.log(n2)
+function somar(){
+    var n1 = parseFloat(document.getElementById('number1').value);
+    var n2 = parseFloat(document.getElementById('number2').value);
+var soma = n1 + n2;
+   console.log(typeof soma);
+   console.log(soma);
+   resultado.innerText = `O resultado da soma é: ${soma}`
+}
+   function sub(){
+    var n1 = parseFloat(document.getElementById('number1').value);
+    var n2 = parseFloat(document.getElementById('number2').value);
+var soma = n1 - n2;
+   console.log(typeof soma);
+   console.log(soma);
+   resultado.innerText = `O resultado da subtração é: ${soma}`
+}
+
+function mult(){
+    var n1 = parseFloat(document.getElementById('number1').value);
+    var n2 = parseFloat(document.getElementById('number2').value);
+var soma = n1 * n2;
+   console.log(typeof soma);
+   console.log(soma);
+   resultado.innerText = `O resultado da multiplicação é: ${soma}`
+}
+
+
+function div() {
+    var n1 = parseFloat(document.getElementById('number1').value);
+    var n2 = parseFloat(document.getElementById('number2').value);
+    if(n2 === 0){
+        resultado.innerText = "Erro: divisão por zero";
+    }
+    else if(isNaN(n2)){
+
+    }
+    else {
+        resultado.innerText = n1 / n2;
+    }
+    
 }
